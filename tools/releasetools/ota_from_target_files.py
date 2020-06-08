5886177e8f65f3fe4e8fc5fe6489613ab6998ab9
user-ilang <33061778+user-ilang@users.noreply.github.com>
2020-06-09 03:29:28 +0700

    Update ota_from_target_files.py

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -751,7 +751,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Print("");
     script.Print(" SDK ver: %s"%(sdkver));
     script.Print("");
-    script.Print(" Root status: Enabled");
+    script.Print(" Root status: Disabled");
     script.Print("");
     script.Print(" Build ID: %s"%(buildidn));
     script.Print("");
@@ -861,16 +861,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
-
-  if os.getenv('WITH_ROOT_METHOD','rootless') == "magisk":
-    if block_based:
-      script.Print(" ")
-      script.Print("Flashing Magisk...")
-      script.Print(" ")
-      common.ZipWriteStr(output_zip, "magisk/magisk.zip",
-                     ""+input_zip.read("SYSTEM/addon.d/magisk.zip"))
-      script.FlashMagisk()
-      script.Print(" ")
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
